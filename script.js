108f721de2c832479381e04a3bc7bf71afb9fa8a
Dylan Rooney <dylantrooney1@gmail.com>
2021-03-16 21:30:32 +1100

    added comments to .js

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Arrays to create the different variables
 var genPassword = [];
-var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz ";
-var numbers = "0123456789 ";
-var specialChar = "!@#$%^&*(){}[]<>?}:; ";
+var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "@", "#", "$" ,"%", "^", "&", "*", "-", "_", "<", ">", ";", ":", "'"]; 
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
+  // Grabs the password Selector
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
   
+  // Sets strings and confirm windows for the client's choice of type of password
   passwordString = "";
-  var useLowerCase = window.confirm("Do you want lower cases?");
-  var useUpperCase = window.confirm("Do you want to use upper cases?");
-  var useNumbers = window.confirm("Do you Want to use Numbers?");
-  var useSpecial = window.confirm("Do you want to use Special Characters?");
+  let useLowerCase = window.confirm("Do you want lower cases?");
+  let useUpperCase = window.confirm("Do you want to use upper cases?");
+  let useNumbers = window.confirm("Do you Want to use Numbers?");
+  let useSpecial = window.confirm("Do you want to use Special Characters?");
 
+  // If the user doesn't choose any characters, the windows will ask again.
   if(useLowerCase == false && useUpperCase == false && useNumbers == false && useSpecial == false) {
     window.alert("Please select one type of character");
-    var useLowerCase = window.confirm("Do you want lower cases?");
-    var useUpperCase = window.confirm("Do you want to use upper cases?");
-    var useNumbers = window.confirm("Do you Want to use Numbers?");
-    var useSpecial = window.confirm("Do you want to use Special Characters?");
+    useLowerCase = window.confirm("Do you want lower cases?");
+    useUpperCase = window.confirm("Do you want to use upper cases?");
+    useNumbers = window.confirm("Do you Want to use Numbers?");
+    useSpecial = window.confirm("Do you want to use Special Characters?");
   }
 
-  var passwordLength = parseInt(pront("Please use a character between 8 and 128"));
-  if(passwordLength < 8 || passwordLength > 128 || typeof(passwordLength) != "number" || passwordLength === null) {
-    window.alert("Password can't be less that 8 characters and more than 128 characters");
+  // Asks client to use a character between 8 and 128
+  let passwordLength = parseInt(window.prompt("Please use a character between 8 and 128"));
+  //checks if the client is using illegitiate characters or something less than 8 characters or more than 128
+  if(passwordLength < 8 || passwordLength > 128 || typeof(passwordLength) != "number" ||passwordLength === NaN || passwordLength === null) {
+    alert("Password can't be less that 8 characters and more than 128 characters");
   }
 
+  // Generates the Password
   if(useLowerCase === true) {
     genPassword.push(lowerCaseChar);
   }
@@ -54,17 +62,20 @@ function writePassword() {
     var randomCharNum;
     var randomchar;
 
+    //randomises the differnt choices
     randomCharArrayNum = parseInt(Math.floor(Math.random()*genPassword.length));
 
-    selectedCharArray=genPassword[randomCharArrayNum];
+    selectedCharArray = genPassword[randomCharArrayNum];
 
     randomCharNum = parseInt(Math.floor(Math.random()*genPassword.length));
 
-    randomchar=genPassword[randomCharNum];
+    randomchar = genPassword[randomCharNum];
 
-    passwordString+=randomchar;
+    //shows the string
+    passwordString += randomchar;
   }
 
+  //redners the final password
   passwordText.textContent = passwordString;
 }
 
